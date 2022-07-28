4719322ee7f24bff181374ab0b7163b5feced6fe
i.kardanov <i.kardanov@wisetech.pro>
2022-07-28 14:05:46 +0300

    написал простого ехо клиента и сервера

--- socket/clients/main.cpp
+++ socket/clients/main.cpp
@@ -1,6 +1,57 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <string>
 #include <iostream>
 
+void errorHandler(int res, const std::string errorMsg) {
+    if (res < 0) {
+        perror(errorMsg.c_str());
+        exit(-1);
+    }
+}
+
 int main(int argc, char **argv) {
-    std::cout << "Client" << std::endl;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errorHandler(sockfd, "socket erorr");
+
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    int res = inet_pton(AF_INET, argv[1], &serverAddress);
+    errorHandler(res, "set server ip address error");
+    serverAddress.sin_port = htons(std::stoi(argv[2]));
+
+    res = connect(sockfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    errorHandler(res, "connect error");
+
+    bool isRunning = true;
+    char buff[1024] = {0};
+    ssize_t n = 0;
+    while (isRunning) {
+        std::cin >> buff;
+        const size_t bufflen = strlen(buff);
+        buff[bufflen] = '\r';
+        buff[bufflen + 1] = '\n';
+
+        const std::string data(buff);
+        if (data == std::string("stop\r\n")) {
+            isRunning = false;
+        }
+
+        n = write(sockfd, buff, strlen(buff));
+        errorHandler(n, "write error");
+        n = read(sockfd, buff, sizeof(buff));
+        errorHandler(n, "read error");
+
+        std::cout << "data from server: " << buff << std::endl;
+        memset(&buff, 0, sizeof(buff));
+    }
+
+    close(sockfd);
+    std::cout << "Close client" << std::endl;
     return 0;
 }
--- socket/servers/simple_server/main.cpp
+++ socket/servers/simple_server/main.cpp
@@ -1,7 +1,80 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <string>
 #include <iostream>
 
+bool isRunServer = true;
+
+void errorHandler(int res, const std::string errorMsg) {
+    if (res < 0) {
+        perror(errorMsg.c_str());
+        exit(-1);
+    }
+}
+
+void handleConnect(int sockfd) {
+    char buff[1024] = {0};
+    ssize_t n = 0;
+    while ((n = read(sockfd, buff, sizeof(buff))) > 0) {
+        const std::string data(buff);
+        if (data == std::string("stop\r\n")) {
+            isRunServer = false;
+            break;
+        }
+
+        n = write(sockfd, buff, sizeof(buff));
+        errorHandler(n, "write error");
+        memset(&buff, 0, sizeof(buff));
+    }
+
+    if (n == 0) {
+        return;
+    } else {
+        errorHandler(n, "read error");
+    }
+}
 
 int main(int argc, char **argv) {
-    std::cout << "simple server!" << std::endl;
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    errorHandler(listenfd, "socket error");
+
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    // для указания номера порта сервера с сетевым порядком байт
+    // нужно использовать только функцию htons!!!
+    serverAddress.sin_port = htons(std::stoi(argv[1]));
+
+    int res = bind(listenfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    errorHandler(res, "bind error");
+
+    std::cout << "Create queue for incoming connection" << std::endl;
+    res = listen(listenfd, 10);
+    errorHandler(res, "listen error");
+
+    char clientIpAddress[INET_ADDRSTRLEN] = {0};
+    struct sockaddr_in clientAddress;
+    socklen_t clientAddressLen = sizeof(clientAddress);
+    while (isRunServer) {
+        memset(&clientAddress, 0, clientAddressLen);
+        int connfd = accept(listenfd, (struct sockaddr*)&clientAddress, &clientAddressLen);
+        errorHandler(connfd, "accept error");
+
+        inet_ntop(AF_INET, &clientAddress, clientIpAddress, clientAddressLen);
+        std::cout << "New client connection with ip: " << clientIpAddress << std::endl;
+        handleConnect(connfd);
+
+        close(connfd);
+        std::cout << "Close client connection" << std::endl;
+    }
+
+    close(listenfd);
+    std::cout << "Stop server" << std::endl;
+
     return 0;
 }
